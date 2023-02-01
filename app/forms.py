8abec282c66bd4ad11894a9ff9a5a07d6a805e2c
Your Name <you@example.com>
2023-02-01 14:13:02 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+g=[('MALE','male'),('FEMALE','female')]
+c=[('python','python'),('django','django'),('sql','sql'),('webtech','webtech')]
+class StudentForm(forms.Form):
+    name=forms.CharField(max_length=100)
+    age=forms.IntegerField(min_value=5)
+    email=forms.EmailField()
+    password=forms.CharField(max_length=100,widget=forms.PasswordInput)
+    address=forms.CharField(max_length=1000,widget=forms.Textarea)
+    gender=forms.ChoiceField(choices=g,widget=forms.RadioSelect)
+    course=forms.MultipleChoiceField(choices=c,widget=forms.CheckboxSelectMultiple)
+
